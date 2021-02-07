b135046eee827c4fdfea1348bd85abe56b25cc30
Micheal Erickson <micheal.erickson3.14@gmail.com>
2021-02-06 20:25:28 -0700

    Add script to remove views
    Maybe we should number these, or consider these as part of the sequential migrations?

---
+++ remove_views.sql
@@ -0,0 +1,13 @@
+DROP VIEW IF EXISTS PageLogin;
+
+DROP VIEW IF EXISTS PageRegister;
+
+DROP VIEW IF EXISTS PageSearchListings;
+
+DROP VIEW IF EXISTS PageUploadHousingUnit;
+
+DROP VIEW IF EXISTS PageRecentFlags;
+
+DROP VIEW IF EXISTS PageViewListing;
+
+DROP VIEW IF EXISTS PageViewOwnListings;
