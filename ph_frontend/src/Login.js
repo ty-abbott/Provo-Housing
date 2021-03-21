c940d2bc2e22a79e9136e31db308bdc7d214043a
Micheal Erickson <micheal.erickson3.14@gmail.com>
2021-03-20 22:26:31 -0600

    src/Login.js

--- ph_frontend/src/Login.js
+++ ph_frontend/src/Login.js
@@ -1,14 +1,77 @@
-import React from "react"
-
-export default function BasicExample() {
-  return (
+import React, { useState } from 'react'
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password:'',
+      dbPassword: ''
+    }
+    this.updateState = this.updateState.bind(this)
+  }
+  updateState(event) {
+    event.preventDefault()
+    console.log(event.target.email.value)
+    console.log(event.target.password.value)
+    console.log("YEET.")
+    console.log(this.state)
+    this.setState({
+      email: event.target.email.value,
+      password: event.target.password.value
+    })
+    console.log(this.state)
+  }
+  componentDidMount(){
+    if (this.state.email != "" && this.state.password != "")
+    {
+      console.log("Ready for the credential check!")
+      var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      var fetchStmt = "http://localhost:8000/User?email=eq." + this.state.email
+      var obj = ""
+      fetch(fetchStmt, requestOptions)
+        .then(response => response.json())
+        .then(json => {
+        console.log(json)
+        this.state.dbPassword = json[0].password
+        /* etc */
+        }).catch(error => {console.error('some error', error)})
+      console.log("userpass: ", this.state.dbPassword)
+    }
+    else
+    {
+      console.log("Waiting on credentials...")
+    }
+  }
+  passCheck()
+  {
+    if ((this.state.password === this.state.dbPassword) && (this.state.password != "")) {
+      console.log("ALL LOGGED IN")
+      window.location.href = '/Search'
+    } else {
+      console.log("Still need more info")
+    }
+  }
+  render() {
+    console.log(this.state)
+    this.componentDidMount()
+    this.passCheck()
+    return (
       <div>
-          <form>
+          <form onSubmit={this.updateState}>
             <h1>Login</h1>
-            <input name="username"/>
-            <input type="password" name="password"/>
-            <button>Go!</button>
+            <label>Email</label>
+            <input name="email" id="email"/>
+            <br></br>
+            <label>Password</label>
+            <input type="password" name="password" id="password"/>
+            <br></br>
+            <button type="submit">Go!</button>
           </form>
       </div>
-  )
+    );
+  }
 }
+export default Login
