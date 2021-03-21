079472340da661f38df513a52f2aad86aa4abbd0
Micheal Erickson <micheal.erickson3.14@gmail.com>
2021-03-20 22:25:56 -0600

    src/Register.js

--- ph_frontend/src/Register.js
+++ ph_frontend/src/Register.js
@@ -1,15 +1,71 @@
-import React from "react"
-
-export default function BasicExample() {
-  return (
+import React, { useState } from 'react'
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.updateState = this.updateState.bind(this)
+  }
+  updateState(event) {
+    event.preventDefault()
+    console.log(event.target.email.value)
+    console.log(event.target.password.value)
+    console.log("YEET.")
+    console.log(this.state)
+    this.setState({
+      email: event.target.email.value,
+      password: event.target.password.value
+    })
+    console.log(this.state)
+    this.componentDidMount()
+  }
+  componentDidMount() {
+    if (this.state.email != "" && this.state.password != "") 
+    {
+      console.log("WE MADE IT!!!")
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      var raw = JSON.stringify({
+        "email":this.state.email,
+        "password":this.state.password,
+        "isadmin":false,
+        "islandlord":false
+      });
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+      };
+      fetch("http://localhost:8000/User", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+    } 
+    else 
+    {
+      console.log("Waiting for full post request...")
+    }    
+  }
+  render() {
+    console.log(this.state)
+    this.componentDidMount()
+    return (
       <div>
-          <form>
-            <h1>Login</h1>
-            <input name="username"/>
-            <input type="password" name="password"/>
-            <input type="password" name="password_c"/>
-            <button>Go!</button>
+          <form onSubmit={this.updateState}>
+            <h1>Register</h1>
+            <label>Email</label>
+            <input name="email" id="email"/>
+            <br></br>
+            <label>Password</label>
+            <input type="password" name="password" id="password"/>
+            <br></br>
+            <button type="submit">Go!</button>
           </form>
-      </div>
-  )
+      </div> 
+    );
+  }
 }
+export default Register
