4378aab91ab686f424fdd39d874aa8b10a5c13f3
Micheal Erickson <micheal.erickson3.14@gmail.com>
2021-03-22 15:07:18 -0600

    Make Login smaller and point to a better place

--- ph_frontend/src/Login.js
+++ ph_frontend/src/Login.js
@@ -5,75 +5,42 @@ class Login extends React.Component {
     super(props);
     this.state = {
       email: '',
-      password:'',
-      dbPassword: ''
+      password: '',
     }
-    this.updateState = this.updateState.bind(this)
+    this.doLogin = this.doLogin.bind(this)
   }
-  updateState(event) {
+
+  doLogin(event) {
     event.preventDefault()
-    console.log(event.target.email.value)
-    console.log(event.target.password.value)
-    console.log("YEET.")
-    console.log(this.state)
-    this.setState({
-      email: event.target.email.value,
-      password: event.target.password.value
-    })
-    console.log(this.state)
-  }
-  componentDidMount(){
-    if (this.state.email != "" && this.state.password != "")
-    {
-      console.log("Ready for the credential check!")
-      var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-      };
-      var fetchStmt = `http://${env.DB_HOST}/User?email=eq.${this.state.email}`
-      var obj = ""
-      fetch(fetchStmt, requestOptions)
-        .then(response => response.json())
-        .then(json => {
+    fetch(`http://${env.DB_HOST}/User?email=eq.${this.state.email}`)
+      .then(response => response.json())
+      .then(json => {
         console.log(json)
-          if (this.state.password === json[0].password) {
-            window.location = "/Search"
-            return
-          }
-          alert('Wrong password')
-        }).catch(error => {console.error('some error', error)})
-      console.log("userpass: ", this.state.dbPassword)
-    }
-    else
-    {
-      console.log("Waiting on credentials...")
-    }
+        if (event.target.password === json[0].password) {
+          this.setState({
+            email: event.target.email.value,
+            password: event.target.password.value
+          })
+          window.location = "/view_housing"
+          return
+        }
+        alert('Wrong password')
+      }).catch(error => { console.error('some error', error) })
   }
-  passCheck()
-  {
-    if ((this.state.password === this.state.dbPassword) && (this.state.password != "")) {
-      console.log("ALL LOGGED IN")
-      window.location.href = '/Search'
-    } else {
-      console.log("Still need more info")
-    }
-  }
+
   render() {
-    console.log(this.state)
-    this.componentDidMount()
-    this.passCheck()
     return (
       <div>
-          <form onSubmit={this.updateState}>
-            <h1>Login</h1>
-            <label>Email</label>
-            <input name="email" id="email"/>
-            <br></br>
-            <label>Password</label>
-            <input type="password" name="password" id="password"/>
-            <br></br>
-            <button type="submit">Go!</button>
-          </form>
+        <form onSubmit={this.doLogin}>
+          <h1>Login</h1>
+          <label>Email</label>
+          <input name="email" id="email" />
+          <br></br>
+          <label>Password</label>
+          <input type="password" name="password" />
+          <br></br>
+          <button type="submit">Go!</button>
+        </form>
       </div>
     );
   }
